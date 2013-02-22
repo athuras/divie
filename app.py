622c0c16d1654979f29bc9d53b8b3ea8b1932f6f
Alexander Huras <alex.huras@gmail.com>
2013-02-22 18:20:29 -0500

    added db boilerplate

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
 import os
 from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+db = SQLAlchemy(app)
+
 
 @app.route('/')
 def hello():
     return 'divie: Hello World!'
+
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
