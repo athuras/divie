b0f533f9c0f9e3ea5700fac8dbb71600bb818e1a
Brian Sinclair <sinclair.brian@gmail.com>
2013-02-23 18:20:55 -0500

    db test

--- divie.py
+++ divie.py
@@ -23,7 +23,7 @@ def home():
 @app.route('/db_test')
 def get_items():
     #query = "SELECT * FROM item;"
-    vals = db.db_test()
+    vals = db.get_items()
     return vals
 
 if __name__ == '__main__':
