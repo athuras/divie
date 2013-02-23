36afc4651bdf70141f46e2ecb36211c789d0d8ea
Brian Sinclair <sinclair.brian@gmail.com>
2013-02-23 17:15:16 -0500

    db test

--- divie.py
+++ divie.py
@@ -5,7 +5,7 @@ from flask import Flask
 
 
 app = Flask(__name__)
-'''
+
 def connect_db():
     conn = psycopg2.connect(**{
             'host': 'ec2-54-243-232-179.compute-1.amazonaws.com',
@@ -15,15 +15,18 @@ def connect_db():
             'port': 5432
             })
     return conn
-'''
+
 @app.route('/')
+def home():
+    return "Not Dead Yet ..."
+'''
 def home():
     db.home()
 
 @app.route('/db_test')
 def get_items():
     db.get_items()
-
+'''
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port, debug=True)
