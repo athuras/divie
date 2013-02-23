ffcd01c7f103890bc61cd792f1e2e209cc95b812
Alexander Huras <alex.huras@gmail.com>
2013-02-23 00:22:30 -0500

    database connection attempt

--- divie.py
+++ divie.py
@@ -3,19 +3,35 @@ import json
 import psycopg2
 
 from flask import Flask
-from flask import render_template
 from flask import request
 
-apo = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
-db = SQLAlchemy(app)
+app = Flask(__name__)
+conn = None
+
+
+def db_init():
+    '''Connects to the PostgreSQL Database'''
+    global conn
+    params = {
+            'host': "'ec2-54-243-232-179.compute-1.amazonaws.com'",
+            'dbname': "'d708fal6ch74uk'",
+            'user': "'qbilqsbasxktlu'",
+            'password': "'vEiXaha5nBimvRAxbjRqygZeSE'",
+            'port': "'5432'"
+            }
+    con_string = ' '.join(k + '+' + v for k, v in params.iteritems())
+    conn = psycopg2.connect(con_string)
 
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    if conn is None:
+        return 'The database connection failed'
+    else:
+        return str(conn)
 
 
 if __name__ == '__main__':
+    db_init()
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port, debug=True)
