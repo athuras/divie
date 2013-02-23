7daf69135584f8119527bc47af4a40441c5dc945
Alexander Huras <alex.huras@gmail.com>
2013-02-22 19:40:56 -0500

    test schema

---
+++ schema.sql
@@ -0,0 +1,58 @@
+/* For example...*/
+
+/*
+Current auctions, and their state information
+*/
+DROP TABLE if exists auction;
+CREATE TABLE auction (
+	auction_id integer PRIMARY KEY AUTOINCREMENT,
+	exec_id integer not null,
+	name string not null,
+	description string not null,
+	start_date date,
+	end_date date
+)
+
+/*
+Current bids
+*/
+DROP TABLE if exists bid;
+CREATE TABLE bid (
+	bid_id integer primary,
+	auction_id integer not null,
+	item_id integer not null,
+	agent_id integer not null,
+	value float null,
+	bid_time int not null
+)
+
+/*
+Implies who is linked to each auction
+*/
+DROP TABLE if exists relationship;
+CREATE TABLE relationship (
+	auction_id integer null,
+	agent_id integer null
+)
+
+/*
+Agent (not user, which is a keyword), the people doing the bidding
+*/
+DROP TABLE if exists agent;
+CREATE TABLE agent (
+	agent_id integer PRIMARY KEY AUTOINCREMENT,
+	name string not null,
+	misc string null
+)
+
+/*
+The items to be bid on
+*/
+DROP TABLE if exists item;
+CREATE TABLE item (
+	item_id integer PRIMARY KEY AUTOINCREMENT,
+	name string null,
+	description string null,
+	img_url string null,
+	value float 0
+)
