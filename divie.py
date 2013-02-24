ad03ca98951a84ceeb0b793ff0de698dd54a03ef
Scott Neil <dscott.neil@gmail.com>
2013-02-23 23:34:35 -0500

    commit

--- divie.py
+++ divie.py
@@ -1,8 +1,7 @@
 import os
 import psycopg2
 import db
-from flask import Flask, url_for, redirect, request, json, Response, jsonify, session, escape
-
+from flask import Flask, url_for, redirect, request, json, Response, jsonify
 
 app = Flask(__name__)
 # app.secret_key = '\xfd{H\xe5<\x95\xf9\xe3\x96.5\xd1\x01O<!\xd5\xa2\xa0\x9fR"\xa1\xa8'
