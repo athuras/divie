e990a2325c6eeb6e6988b4ea8e545f7befb81d22
Scott Neil <dscott.neil@gmail.com>
2013-02-24 16:31:58 -0500

    add saving functionality of bids

--- db.py
+++ db.py
@@ -116,13 +116,14 @@ def user_auc_rel(): #find which users are associated with the current auction
     vals = query_template(query)
     return str(vals)
 
-# def save_Bids(results, userID):
-#     auction_id = 1
+def save_Bids(results, userID):
+    auction_id = 1
 
-#     for curResult in results:
-#         if curResult.value != 0 #look for better way
-#             query = "INSERT INTO bid VALUES ({0}, {1}, {2}, {3}, {4})".format(auction_id, curResult.item_id, userID, curResult.value, 1)
-#             vals = query_template(query)
+    for curResult in results:
+        if curResult.value != 0 #look for better way
+            query = "INSERT INTO bid VALUES ({0}, {1}, {2}, {3}, {4})" \
+                .format(auction_id, curResult.id, userID, curResult.rank, 1)
+            vals = query_template(query)
 
-#     #figure out what to do here
-#     return "successful"
+    #figure out what to do here
+    return "successful"
--- divie.py
+++ divie.py
@@ -8,7 +8,7 @@ app.secret_key = '\xfd{H\xe5<\x95\xf9\xe3\x96.5\xd1\x01O<!\xd5\xa2\xa0\x9fR"\xa1
 
 @app.route('/')
 def home():
-    return "test"# redirect(url_for('static', filename='login.html'))
+    return redirect(url_for('static', filename='login.html'))
 
 @app.route('/static/login.html', methods=['POST'])
 def login():
@@ -35,18 +35,18 @@ def getItems():
         resp = Response(js, status=200, mimetype='application/json')
         return resp
 
-# @app.route('/submitBids', methods=['POST'])
-# def auction():
-#     # When user has completed rankings insert into database and return succesful
-#     if request.method == 'POST':
-#         result = response.data
-#         saveResult = db.save_Bids(result, escape(session['username']))
-#         # #TODO: Insert into BIds table
-#         # #TODO: redirect to myAuctions page if successful
-#         if saveResult == "successful"
-#             return "success"#redirect(url_for('static', filename='myAuctions.html'))
-#         else 
-#             return "ERROR"
+@app.route('/submitBids', methods=['POST'])
+def auction():
+    # When user has completed rankings insert into database and return succesful
+    if request.method == 'POST':
+        result = response.data
+        saveResult = db.save_Bids(result, escape(session['username']))
+        # #TODO: Insert into BIds table
+        # #TODO: redirect to myAuctions page if successful
+        if saveResult == "successful"
+            return saveResult#redirect(url_for('static', filename='myAuctions.html'))
+        else 
+            return "ERROR"
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
