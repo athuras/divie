3324152705363a0f8d4426d05f781647c92e09a6
Scott Neil <dscott.neil@gmail.com>
2013-02-23 20:00:56 -0500

    adding imgs

--- static/script/loadElements.js
+++ static/script/loadElements.js
@@ -19,7 +19,7 @@ function loaded()
 		async: false,
 		success: function(data){ 
 			$.each(data, function(i, at){
-				AssetList.push(new Asset(at.id, at.name, at.ranking, at.desc));
+				AssetList.push(new Asset(at.id, at.name, at.ranking, at.desc, at.img));
 			});
 
 			addAssets();
