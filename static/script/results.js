29c0313d06fa409ca1ba6727660915b2879431ff
Matthew Chong <matt.chong45@gmail.com>
2013-03-01 14:03:26 -0500

    added in loadResults and createFooter in results.js

--- static/script/results.js
+++ static/script/results.js
@@ -1,13 +1,43 @@
-var AuctionList = [];
-AuctionList.push(new Auction(1, 1, "Grandma's Belongings", "Splittin her shit up", "February 23, 2013", "March 2, 2013"));
-AuctionList.push(new Auction(1, 1, "Uncle Ken's Shit Needs Divie", "Splittin her shit up", "April 23, 2013", "April 30, 2013"));
+// Initialization stuff ------------------------------------------------------------------------------
 
+var AssetList = [];
+AssetList.push(new Asset(101, "Sailboat Painting", 0, '', "img/sailboat.png"));
+AssetList.push(new Asset(2, "Car", 0, '', "img/car.jpg"));
+AssetList.push(new Asset(3, "Lamp", 0, '', "img/sailboat.png"));
+AssetList.push(new Asset(4, "Family Portrait", 0, '', "img/sailboat.png"));
+AssetList.push(new Asset(5, "Sailboat Painting", 0, '', "img/sailboat.png"));
+AssetList.push(new Asset(6, "Everything Else", 0, '', "img/sailboat.png"));
+AssetList.push(new Asset(1031, "Sailboat Painting", 0, '', "img/sailboat.png"));
+AssetList.push(new Asset(23, "Car", 0, '', "img/car.jpg"));
+AssetList.push(new Asset(33, "Lamp", 0, '', "img/sailboat.png"));
+AssetList.push(new Asset(43, "Family Portrait", 0, '', "img/sailboat.png"));
+AssetList.push(new Asset(53, "Sailboat Painting", 0, '', "img/sailboat.png"));
+AssetList.push(new Asset(36, "Everything Else", 0, '', "img/sailboat.png"));
 
-var results = [];
-results.push();
+var ResultList = [];
+ResultList.push(new Result(101, [0,1,1,0]));
+ResultList.push(new Result(2, [1,1,1,0]));
+ResultList.push(new Result(3, [0,1,1,0]));
+ResultList.push(new Result(4, [0,0,0,0]));
+ResultList.push(new Result(5, [0,1,0,0]));
+ResultList.push(new Result(6, [0,1,1,0]));
+ResultList.push(new Result(1031, [0,1,1,1]));
+ResultList.push(new Result(23, [0,1,1,0]));
+ResultList.push(new Result(33, [1,1,1,1]));
+ResultList.push(new Result(43, [1,1,1,0]));
+ResultList.push(new Result(53, [0,0,0,0]));
+ResultList.push(new Result(36, [0,0,1,0]));
 
-var NUM_OF_LOTS = 3;
+// Initialization stuff ------------------------------------------------------------------------------
 
+var NUM_OF_LOTS = 0;
+
+function getNumOfLots(resultList) {
+	if (resultList.length == 0)
+		alert("Err: No Results");
+	else
+		NUM_OF_LOTS = resultList[0].lots.length;
+};
 
 function Asset (id, name, rank, description, imgSrc){
 	this.id = id;
@@ -18,13 +48,28 @@ function Asset (id, name, rank, description, imgSrc){
 	this.img = "img/sailboat.png";//imgSrc;
 };
 
-function Result()
+function Result(id, lots)
 {
-
+	this.lots = lots;
+	this.asset = AssetList[findArrLoc(id)];
 };
+
+function findArrLoc(idTag)
+{
+	for(var i=0; i<AssetList.length;i++)
+	{
+		if (AssetList[i].id == idTag)
+		{
+			return i;
+		}
+	}
+};
+
 
 function buildResultsTable()
 {
+	getNumOfLots(ResultList);
+
 	var head = document.getElementById("head");
 
 	for(var i=0;i<NUM_OF_LOTS+1;i++)
@@ -49,10 +94,61 @@ function buildResultsTable()
 		newCol.appendChild(colTitle);
 
 		head.appendChild(newCol);
-	}
+	};
+
+	loadResults();
 };
 
 function loadResults()
 {
+	var body = document.getElementById("body");
 
+	for (var i = 0; i < ResultList.length; i++) {
+		console.log(ResultList.length)
+		var newAssetRow = document.createElement("tr");
+		newAssetRow.setAttribute("class", "assetRow");
+
+		var newAssetName = document.createElement("td");
+		newAssetName.setAttribute("class", "assetName");
+		var assetName = document.createTextNode(ResultList[i].asset.name);	
+		newAssetName.appendChild(assetName);
+
+		newAssetRow.appendChild(newAssetName);
+
+		for (var j = 0; j < NUM_OF_LOTS; j++) {
+			var newIcon = document.createElement("td");
+			newIcon.setAttribute("class", getIcon(ResultList[i].lots[j]));
+
+			newAssetRow.appendChild(newIcon);
+		};
+
+		body.appendChild(newAssetRow);
+	};
+
+	createFooter();
+};
+
+function createFooter(){
+	var foot = document.getElementById("foot");
+
+	for (var i = 0; i < NUM_OF_LOTS+1; i++) {
+		var newRow = document.createElement("tr");
+		var newTd = document.createElement("td");
+		var newImg = document.createElement("img");
+		newImg.setAttribute("src", "img/approve-btn.png")
+		if (i != 0)
+		{
+			newTd.appendChild(newImg);
+		}
+
+		newRow.appendChild(newTd);
+	};
+};
+
+function getIcon(value)
+{
+	if (value == 0)
+		return "icon-cancel";
+	else
+		return "icon-checkmark";
 };
