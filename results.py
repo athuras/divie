b17718b88685c551e41386dde7f7fb04918484e7
Scott Neil <dscott.neil@gmail.com>
2013-03-10 00:31:40 -0500

    JUST FOR MATT: just killin it.

--- results.py
+++ results.py
@@ -2,7 +2,7 @@ def processResults(iterable, aucLots, auction_id=1):
 	'''iterable = [(item, user, auction, lot)] sorted by <auction, user, item>'''
 	'''aucLots = [array of all auctions]'''
 	def checkLot(aux, lot):
-		if (lot in aux):
+		if (lot[0] in aux):
 			return 1
 		else:
 			return 0
@@ -46,6 +46,12 @@ def processResults(iterable, aucLots, auction_id=1):
 # 		'item_id': 1, 
 # 		'img_url': '11', 
 # 		'item_name': "thing1", 
+# 		'lot_id': 0},
+# 		{'auction_id': 1, 
+# 		'agent_id': 1, 
+# 		'item_id': 1, 
+# 		'img_url': '11', 
+# 		'item_name': "thing1", 
 # 		'lot_id': 1},
 # 		{'auction_id': 1, 
 # 		'agent_id': 1, 
@@ -55,32 +61,27 @@ def processResults(iterable, aucLots, auction_id=1):
 # 		'lot_id': 2},
 # 		{'auction_id': 1, 
 # 		'agent_id': 1, 
-# 		'item_id': 3, 
-# 		'img_url': '11', 
-# 		'item_name': "thing1", 
-# 		'lot_id': 3},
-# 		{'auction_id': 1, 
-# 		'agent_id': 2, 
-# 		'item_id': 1, 
-# 		'img_url': '11', 
-# 		'item_name': "thing1", 
-# 		'lot_id': 2},
-# 		{'auction_id': 1, 
-# 		'agent_id': 2, 
-# 		'item_id': 1, 
-# 		'img_url': '11', 
-# 		'item_name': "thing1", 
-# 		'lot_id': 3},
-# 		{'auction_id': 1, 
-# 		'agent_id': 3, 
-# 		'item_id': 1, 
-# 		'img_url': '11', 
-# 		'item_name': "thing1", 
-# 		'lot_id': 3},{'auction_id': 1, 
-# 		'agent_id': 3, 
 # 		'item_id': 2, 
+# 		'img_url': '11', 
+# 		'item_name': "thing1", 
+# 		'lot_id': 0},
+# 		{'auction_id': 1, 
+# 		'agent_id': 1, 
+# 		'item_id': 2, 
+# 		'img_url': '11', 
+# 		'item_name': "thing1", 
+# 		'lot_id': 1},
+# 		{'auction_id': 1, 
+# 		'agent_id': 3, 
+# 		'item_id': 1, 
+# 		'img_url': '11', 
+# 		'item_name': "thing1", 
+# 		'lot_id': 0},
+# 		{'auction_id': 1, 
+# 		'agent_id': 3, 
+# 		'item_id': 1, 
 # 		'img_url': '11', 
 # 		'item_name': "thing1", 
 # 		'lot_id': 1}]
 
-# print processResults(testvals, [1,2,3])
+# print processResults(testvals, [0,1,2])
