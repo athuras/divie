b8c85a5abd84ec2aa66cb95b126d2eb7193db9c1
Scott Neil <dscott.neil@gmail.com>
2013-03-10 21:45:58 -0400

    added item name to all bids query:

--- db.py
+++ db.py
@@ -216,7 +216,7 @@ def get_allBids(auction_id=1):
     users = ("SELECT * FROM agent;")
     allUs = query_template_dict(users)
     bids = ("SELECT bid.agent_id, bid.value, item.item_name FROM bid INNER JOIN item ON"+
-            "bid.item_id = item.item_id and bid.auction_id = %(aucID)s;")
+            " bid.item_id = item.item_id and bid.auction_id = %(aucID)s;")
     allBs = query_template_dict(bids, {"aucID": auction_id})
     combined = [{"agent_id": u['agent_id'], "agent_name": u['agent_name'], "profile": "img/"+u['profile'], 
             "Bids": [bid for bid in allBs if bid['agent_id']==u['agent_id']]} for u in allUs]
